f0b6a6f6bfdc93981d0797c34d06f37a9ad9ad49
Steffen Elberg Godskesen <sego@dtic.dtu.dk>
2012-07-31 16:38:42 +0200

    Enable stubbing of DTUbasen and CAS in development mode
    To enable toshokan to run without a network connection (or without
    access to the DTU network) it is now  possible to stub out DTUbasen
    and CAS by setting
    
        Toshokan::Application.config.stub_authentication=true
    
    (e.g. in config/application.local.rb). This makes OmniAuth and
    Dtubase cooperate to always log in as a user with identifier "0"

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,16 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :host => Toshokan::Application.config.cas[:host],
     :ssl => true
 end
+
+if Toshokan::Application.config.stub_authentication
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.add_mock(:cas, {
+    :uid => "username",
+    :info => { :name => "Test User" },  
+    :extra => {
+      :norEduPerson => [{
+        :norEduPersonLIN => "0"
+      }]
+    }
+  })
+end
