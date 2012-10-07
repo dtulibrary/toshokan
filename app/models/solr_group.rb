25720fa4626f68b5e5a009a82daeabfd99d3c357
Rikke Willer <riwi@dtic.dtu.dk>
2012-10-07 12:26:57 +0200

    Group member selection and basic test

--- app/models/solr_group.rb
+++ app/models/solr_group.rb
@@ -1,27 +1,39 @@
 # -*- encoding : utf-8 -*-
 class SolrGroup 
   extend ActiveModel::Naming
+  include RSolr::Ext::Doc
+
+  @@source_type_priority = {
+    'research' => 1,
+    'publisher' => 2,
+    'openaccess' => 3,
+    'database' => 4,
+    'aggregator' => 5,
+    'unknown' => 6      
+  }
 
   def initialize(group_values, solr_response)
     
     @group_values = group_values
     @solr_response = solr_response
 
+    # select the member which will be used for display
+    @member = primary_member
+  end
+
+  def member_id
+    @member.data["id"]
   end  
-  
+
   def id
     @group_values["groupValue"]
   end
 
   def [] key
-    @group_values["doclist"]["docs"].first[key]
+    @member.data[key.to_s]
   end    
   
-  def get(key, options = {})
-    self[key]
-  end  
-
-  def has?(key)
+  def key?(key)
     self[key].present?
   end  
   
@@ -31,6 +43,30 @@ class SolrGroup
   
   def export_formats
     {}
+  end
+
+  private
+
+  def primary_member
+    @members = Array.new
+    Rails.logger.debug("dedup #{@group_values[:doclist.to_s].class}")
+    @group_values["doclist"]["docs"].each do |doc|
+      @members.push GroupMember.new(doc, doc[:source_type])
+    end
+    @members.sort! {|x,y| @@source_type_priority[x.source_type] <=> @@source_type_priority[y.source_type]}
+    @members.first
+  end  
+
+  class GroupMember
+
+    attr_reader :data
+    attr_reader :source_type
+
+    def initialize(data, source_type)
+      @data = data
+      @source_type = source_type
+    end
+
   end  
 
 end  
