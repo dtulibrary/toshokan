9f33e4e0738b6bcb568adb8d8d764e97cffba62b
Steffen Elberg Godskesen <sego@dtic.dtu.dk>
2012-11-29 00:50:20 +0100

    Adapt pagination features for Blacklight 4.0
      - re-enable pagination tests
      - adapt cucumber steps to match new markup

--- features/paginate.feature
+++ features/paginate.feature
@@ -1,4 +1,3 @@
-@wip
 Feature: Paginate search results
 
   In order to browse through the entire search result
@@ -14,33 +13,32 @@ Feature: Paginate search results
 
   Scenario: A result set that fits within the inner window of the pagination
     When I search for "technology"
-    Then I should see 5 page links
-    And  I should see the next page link
-    But  I should not see the previous page link
+    Then I should see page links from 1 to 5
+    And  the next page link should be active
+    But  the previous page link should be inactive
     And  I should not see any page gaps
 
   Scenario: A result set that doesn't fit within the inner window of the pagination
     When I search for "*"
-    Then I should see 5 page links
-    And  I should see the next page link
+    Then I should see page links from 1 to 5
     And  I should see the forward page gap
-    But  I should not see the previous page link
-    And  I should not see the backward page gap
+    And  the next page link should be active
+    But  the previous page link should be inactive
 
   Scenario: Moving into a result set that fits within the inner window of the pagination
     When I search for "technology"
     And  I go to the next page of the result set
-    Then I should see 5 page links
-    And  I should see the previous page link
-    And  I should see the next page link
+    Then I should see page links from 1 to 5
+    And  the next page link should be active
+    And  the previous page link should be active
     But  I should not see any page gaps
 
   Scenario: Moving into a result set that doesn't fit within the inner window of the pagination
     When I search for "*"
     And  I go to page 5 of the result set
     And  I go to the next page of the result set
-    Then I should see 9 page links
-    And  I should see the previous page link
-    And  I should see the next page link
+    Then I should see page links from 2 to 10
+    And  the next page link should be active
+    And  the previous page link should be active
     And  I should see both page gaps
 
--- features/step_definitions/paginate_steps.rb
+++ features/step_definitions/paginate_steps.rb
@@ -1,42 +1,44 @@
+# encoding: utf-8
+
 Then /^I should not see any pagination$/ do
-  page.has_css?('.pagination-container').should be_false
+  page.has_css?('.pagination').should be_false
 end
 
-Then /^I should see (\d+) page links$/ do |number_of_pages|
-  page_gaps = page.all('.page_links .page.gap').size
-
-  # Double up on number_of_pages since there is a page selector above 
-  # and below document list.
-  (page.all('.page_links .page').size - page_gaps).should == 2 * number_of_pages.to_i
+Then /^I should see page links from (\d+) to (\d+)$/ do |from, to|
+  (from.to_i..to.to_i).each{ |page_number|
+    page.has_css?('.pagination ul li a', :text => "#{page_number}").should be_true
+  }
+end
+Then /^the (next|previous) page link should be active$/ do |link_type|
+  find('.pagination ul li', :text => (link_type == 'next' ? 'Next »' : '« Previous'))[:class].should_not include('disabled')
 end
 
-Then /^I should((?:n't| not))? see the page gap$/ do |negate|
-  page.has_css?('.page.gap').should (negate ? be_false : be_true)
+Then /^the (next|previous) page link should be inactive$/ do |link_type|
+  find('.pagination ul li', :text => (link_type == 'next' ? 'Next »' : '« Previous'))[:class].should include('disabled')
 end
 
-Then /^I should((?:n't| not))? see the (next|previous) page link$/ do |negate, link_type|
-  # Next and previous links have CSS classes 'next_page' and 'prev_page'.
-  # When they're disabled, they also get CSS class 'disabled'.
-  page.has_css?(".#{link_type == 'next' ? 'next' : 'prev'}_page#{negate ? '.disabled' : ''}").should be_true
+Then /^I should see the (forward|backward) page gap$/ do |gap_type|
+  #debugger
+  all('.pagination ul li a')[gap_type == 'forward' ? -1 : -2].text.should =='…'
 end
 
-Then /^I should((?:n't| not))? see the (forward|backward) page gap$/ do |negate, gap_type|
-  gap_selector = '.page.gap:' + (gap_type == 'forward' ? 'last-child' : 'first-child')
-  page.has_css?(gap_selector).should (negate ? be_false : be_true)
-end
-
-Then /^I should(?:n't| not) see any page gaps$/ do 
-  page.has_css?('.page.gap').should be_false
+Then /^I should not see the (forward|backward) page gap$/ do |gap_type|
+  find('.pagination ul li a')[gap_type == 'forward' ? -1 : 2].text.should_not == '…'
 end
 
 Then /^I should see both page gaps$/ do
-  page.all('.page.gap:first-child, .page.gap:last-child').size.should == 4
+  all('.pagination ul li a')[2].text.should == '…'
+  all('.pagination ul li a')[-1].text.should == '…'
+end
+
+Then /^I should not see any page gaps$/ do
+  page.should_not have_css('.pagination ul li a', :text => '...')
 end
 
 When /^I go to page (\d+) of the result set$/ do |page_number|
-  page.find('.page a', :text => page_number).click
+  page.find('.pagination a', :text => page_number).click
 end
 
 When /^I go to the (next|previous) page of the result set$/ do |link_type|
-  page.find(".#{link_type == 'next' ? 'next' : 'prev'}_page a").click
+  click_link (link_type == 'next' ? 'Next »' : '« Previous')
 end
