551fed3f0cb91bbf3691a982f03048a48f21958b
Jimmy Petersen <jipe@dtic.dtu.dk>
2013-01-23 09:05:41 +0100

    Schema.rb updated by migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121130090034) do
+ActiveRecord::Schema.define(:version => 20130122155543) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
