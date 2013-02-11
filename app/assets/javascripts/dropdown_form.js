0e95b69edbd6ef15a9f4abeea770b7425f812f1d
Steffen Elberg Godskesen <sego@dtic.dtu.dk>
2013-02-11 17:00:22 +0100

    Cleanup inconsistencies and whitespace in javascript files

--- app/assets/javascripts/dropdown_form.js
+++ app/assets/javascripts/dropdown_form.js
@@ -1,5 +1,7 @@
-$(function(){
+// allows form elements inside a bootstrap dropdown recieve input
+// without this, the dropdown would close when clicking the form elements
+(function($){
   $('.dropdown.form form input,label').on('click', function (e) {
     e.stopPropagation();
   });
-});
+})(jQuery);
--- app/assets/javascripts/spin.jquery.js
+++ app/assets/javascripts/spin.jquery.js
@@ -1,19 +1,19 @@
 (function ($) {
 
-	$.fn.spin = function(opts) {
-	  this.each(function() {
-	    var $this = $(this),
-	        data = $this.data();
+  $.fn.spin = function(opts) {
+    this.each(function() {
+      var $this = $(this),
+      data = $this.data();
 
-	    if (data.spinner) {
-	      data.spinner.stop();
-	      delete data.spinner;
-	    }
-	    if (opts !== false) {
-	      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
-	    }
-	  });
-	  return this;
-	};
+      if (data.spinner) {
+        data.spinner.stop();
+        delete data.spinner;
+      }
+      if (opts !== false) {
+        data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+      }
+    });
+    return this;
+  };
 
 })(jQuery);
