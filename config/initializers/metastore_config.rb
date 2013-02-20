1516eefb54be8f39fbb86c5baeefe18778d7624d
Rikke Willer <riwi@dtic.dtu.dk>
2013-02-20 10:43:36 +0100

    Upgrade solr test setup to 4.1

--- config/initializers/metastore_config.rb
+++ config/initializers/metastore_config.rb
@@ -2,7 +2,7 @@
 Toshokan::Application.config.metastore = {
   :name => "metastore-test",
   :version => "1.0-SNAPSHOT",
-  :solr_version => "4.0",
+  :solr_version => "4.1",
   :group => "dk/dtu/dtic",
   :maven_local_path => "#{ENV['HOME']}/.m2/repository/",
   :maven_dtic_path => "http://maven.cvt.dk/"
