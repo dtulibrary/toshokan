c218f76853525d25596f3f63c3860c7346aed282
Steffen Elberg Godskesen <sego@dtic.dtu.dk>
2013-08-18 20:37:03 +0200

    Send omniauth log messages to Rails logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            :setup => true
 end
 
+OmniAuth.config.logger = Rails.logger
+
 if Rails.application.config.auth[:stub]
   OmniAuth.config.test_mode = true
   OmniAuth.config.add_mock(:cas, {
