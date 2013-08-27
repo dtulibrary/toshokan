087e3f1ddfceae75950b5296a25ac4eaf612f20c
Rikke Willer <riwi@dtic.dtu.dk>
2013-08-27 14:55:40 +0200

    Add support for media queries in IE8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 
+//= require 'respond.min.js'
+
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
 // this:
 //= require 'blacklight_range_limit'
