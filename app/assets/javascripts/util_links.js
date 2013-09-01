e6c18e6071f7db4038cb9b10d4041b1757990579
Rikke Willer <riwi@dtic.dtu.dk>
2013-09-01 17:04:32 +0200

    Removing js console stmts (IE doesn't like them) + reintroduce icon font

--- app/assets/javascripts/util_links.js
+++ app/assets/javascripts/util_links.js
@@ -2,10 +2,8 @@
     $(function() {
         if ($('#util-links').is(":visible")) {
             offset = $('#util-links').offset();
-            console.log(offset);
             offset.top += 30;
             offset.left = offset.left - 135;
-            console.log(offset);
 
             $('#login-popover').show();        
             $('#login-popover').offset(offset);
