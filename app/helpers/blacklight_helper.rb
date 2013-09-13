4f8ed95067b1b53161c3e3d8974fba7053514243
Rikke Willer <riwi@dtic.dtu.dk>
2013-09-13 14:57:31 +0200

    Quick fix to error handling for displaying invalid search query parameters (this should be replaced by a more robust render_search_to_s function)

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -52,8 +52,13 @@ module BlacklightHelper
 
   # Search History and Saved Searches display
   def link_to_search_history_item(params, html="")
-    params.delete(:t)
-    link_to(raw(html+render_search_to_s(params)), catalog_index_path(params)).html_safe
+    begin 
+      params.delete(:t)      
+      link_to(raw(html+render_search_to_s(params)), catalog_index_path(params)).html_safe
+    rescue
+      Rails.logger.warn("Search url could not be rendered from #{params.inspect}")  
+      ""
+    end
   end
 
   def render_search_bar
