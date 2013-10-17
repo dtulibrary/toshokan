63cbd48228018b0576c1baa2cbf4cedb8c39a563
Steffen Elberg Godskesen <sego@dtic.dtu.dk>
2013-10-17 08:46:58 +0200

    Disable ToC UI in staging

--- config/features.rb
+++ config/features.rb
@@ -2,12 +2,15 @@
 FeatureFlipper.features do
   
   in_state :development do
-    feature :alert, :description => "Journal and search alerts"
+    feature :alert,   :description => "Journal and search alerts"
+  end
+
+  in_state :unstable do
+    feature :toc,     :description => "Display table of contents on journal records"
   end
 
   in_state :staging do
     feature :nal_map, :description => "Show Google Maps for NAL"
-    feature :toc,     :description => "Display table of contents on journal records"
   end
 
   in_state :live do    
@@ -17,6 +20,7 @@ end
 
 FeatureFlipper::Config.states = {
   :development => ['development', 'test'].include?(Rails.env),
+  :unstable    => ['development', 'test', 'unstable'].include?(Rails.env),
   :staging     => ['development', 'test', 'unstable', 'staging'].include?(Rails.env),
   :live        => true
 }
