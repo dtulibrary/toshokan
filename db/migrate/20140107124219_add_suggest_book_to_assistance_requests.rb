cfc50cb2119b919588a04957c0ccba5f53925ed5
Jimmy Petersen <jipe@dtic.dtu.dk>
2014-01-08 14:20:21 +0100

    Remove manual SQL from migration. Refs #1440

--- db/migrate/20140107124219_add_suggest_book_to_assistance_requests.rb
+++ db/migrate/20140107124219_add_suggest_book_to_assistance_requests.rb
@@ -1,10 +1,6 @@
 class AddSuggestBookToAssistanceRequests < ActiveRecord::Migration
   def up
     add_column :assistance_requests, :book_suggest, :boolean, :default => false
-
-    AssistanceRequest.connection.execute %q{
-      update assistance_requests set book_suggest = false;
-    }
   end
 
   def down
