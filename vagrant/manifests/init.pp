a965acea3a572e318c42d7f16dcc03507f10c003
Steffen Elberg Godskesen <steffen@g7n.dk>
2014-11-01 22:04:30 +0100

    Preparations for blacklight 5 upgrade

--- vagrant/manifests/init.pp
+++ vagrant/manifests/init.pp
@@ -4,8 +4,9 @@ class {'apache2':
   disable_default_vhost => true,
 }
 
-class {'toshokan': 
-  rails_env  => 'unstable',
-  conf_set   => 'vagrant',
-  vhost_name => 'toshokan.vagrant.vm',
+class {'toshokan':
+  rails_env       => 'unstable',
+  conf_set        => 'vagrant',
+  vhost_name      => 'toshokan.vagrant.vm',
+  bundler_version => 'cvt_provided',
 }
