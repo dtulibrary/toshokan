cdabacd6c33d374f8b05ca963b8962459792952f
Jimmy Petersen <jipe@dtic.dtu.dk>
2014-12-09 14:48:33 +0100

    Sending cover images as inline attachments with content type image/png. Refs #1977

--- app/controllers/cover_images_controller.rb
+++ app/controllers/cover_images_controller.rb
@@ -3,7 +3,7 @@ class CoverImagesController < ApplicationController
   def show 
     id = params[:id]
     response = CoverImages.get id
-    send_data response.body, :status => response.code
+    send_data response.body, :status => response.code, :disposition => 'inline', :type => 'image/png'
   end
 
 end
