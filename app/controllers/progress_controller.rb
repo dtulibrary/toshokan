ea8e02d7ac7f375de5775b18814123e25f5e1e3a
Steffen Elberg Godskesen <steffen@g7n.dk>
2014-12-16 08:54:52 +0100

    Spec progress_controller

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -2,7 +2,7 @@ class ProgressController < ApplicationController
   def show
     respond_to do |format|
       format.json do
-        p = Progress.find_by_name(params[:name])
+        p = Progress.find_by_name(params[:name]) || not_found
         render :json => p.to_json
       end
     end
--- spec/controllers/progress_controller_spec.rb
+++ spec/controllers/progress_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 describe ProgressController do
+  describe '#show' do
+    it 'returns json describing progress' do
+      name = 'I think'
+      Progress.create(:name => name)
+      xhr :get, :show, :format => :json, :name => name
+      expect(response).to be_success
+      expect(JSON.parse(response.body)).to be_a(Hash)
+    end
+
+    context 'when progress object does not exist' do
+      it 'returns 404' do
+        xhr :get, :show, :format => :json, :name => 'I don\'t exist'
+        expect(response).to be_not_found
+      end
+    end
+
+  end
 
 end
