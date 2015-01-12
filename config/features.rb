dca5d7129a6fba0cafe26aaaf7397ba9f6541bbe
Jimmy Petersen <jipe@dtic.dtu.dk>
2015-01-12 09:16:43 +0100

    Enable mendeley feature on staging. Refs #2031
    - Also removes feature toggling from features that have long since
      been added to production.

--- config/features.rb
+++ config/features.rb
@@ -5,16 +5,13 @@ FeatureFlipper.features do
   end
 
   in_state :unstable do
-    feature :mendeley, :description => "Use Mendeley API instead of Web Importer"
   end
 
   in_state :staging do
-    feature :nal_map, :description => "Show Google Maps for NAL"
+    feature :mendeley, :description => "Use Mendeley API instead of Web Importer"
   end
 
   in_state :live do
-    feature :book_suggest, :description => 'Enable users to suggest books for acquistion'
-    feature :cant_find_it, :description => 'Display sidebar links to Cant find forms'
     feature :alis, :description => "Show loan info from Alis"
     feature :cff_resolver, :description => "Use OpenURL resolver in CFFs"
   end
