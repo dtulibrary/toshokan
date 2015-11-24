5b37073999b25b3066ef9233e2c0c0690c2d490f
Matt Zumwalt <matt@databindery.com>
2015-11-24 13:31:49 +0100

    Rely on dtu_blacklight_common for shared behaviors. Remove redundant code.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,18 +4,16 @@ require 'i18n'
 class CatalogController < ApplicationController
   layout 'with_search_bar'
 
-  include Blacklight::Catalog
+  include Dtu::CatalogBehavior
   include Toshokan::Catalog
 
   before_filter :inject_last_query_into_params, only:[:show]
 
   configure_blacklight do |config|
-    # Ensure I18n load paths are loaded
-    Dir[Rails.root + 'config/locales/**/*.{rb,yml}'].each { |path| I18n.load_path << path }
 
     config.solr_path = 'toshokan'
     config.document_solr_path = 'toshokan_document'
-    config.document_presenter_class = Dtu::DocumentPresenter
+    # config.document_presenter_class = Dtu::DocumentPresenter
     config.metrics_presenter_classes = [Dtu::Metrics::AltmetricPresenter, Dtu::Metrics::IsiPresenter, Dtu::Metrics::DtuOrbitPresenter, Dtu::Metrics::PubmedPresenter]
 
     # Set resolver params
@@ -55,7 +53,7 @@ class CatalogController < ApplicationController
     }
 
     # Set per page options
-    config.per_page = [10, 20, 50]
+    # config.per_page = [10, 20, 50]
     config.max_per_page = 500
 
     # solr field configuration for search results/index views
@@ -66,39 +64,19 @@ class CatalogController < ApplicationController
     config.show.title_field = 'title_ts'
     config.show.display_type_field = 'format'
 
+    ##
+    # FACET FIELDS
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
     #
-    # Setting a limit will trigger Blacklight's 'more' facet values link.
-    # * If left unset, then all facet values returned by solr will be displayed.
-    # * If set to an integer, then "f.somefield.facet.limit" will be added to
-    # solr request, with actual solr request being +1 your configured limit --
-    # you configure the number of items you actually want _displayed_ in a page.
-    # * If set to 'true', then no additional parameters will be sent to solr,
-    # but any 'sniffed' request limit parameters will be used for paging, with
-    # paging at requested limit -1. Can sniff from facet.limit or
-    # f.specific_field.facet.limit solr request params. This 'true' config
-    # can be used if you set limits in :default_solr_params, or as defaults
-    # on the solr side in the request handler itself. Request handler defaults
-    # sniffing requires solr requests to be made with "echoParams=all", for
-    # app code to actually have it echo'd back to see it.
-    #
-    # :show may be set to false if you don't want the facet to be drawn in the
-    # facet bar
-    #config.add_facet_field 'format', :collapse => false
     config.add_facet_field 'format', :collapse => false
     #config.add_facet_field 'subformat_s', :collapse => false
     config.add_facet_field 'pub_date_tsort', :label => I18n.t('blacklight.search.fields.facet.pub_date_tsort'), :range => true
     config.add_facet_field 'author_facet', :limit => 20
     config.add_facet_field 'journal_title_facet', :limit => 20
 
-    # Have BL send all facet field names to Solr, which has been the default
-    # previously. Simply remove these lines if you'd rather use Solr request
-    # handler defaults, or have no facets.
-    config.add_facet_fields_to_solr_request!
-    #use this instead if you don't want to query facets marked :show=>false
-    #config.default_solr_params[:'facet.field'] = config.facet_fields.select{ |k, v| v[:show] != false}.keys
-
+    ##
+    # INDEX FIELDS
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
     config.add_index_field 'author_ts', :helper_method => :render_shortened_author_links, :highlight => true
@@ -110,10 +88,12 @@ class CatalogController < ApplicationController
     #config.add_index_field 'subformat_s', :helper_method => :render_subtype
     config.add_index_field 'doi_ss'
     config.add_index_field 'publisher_ts', :format => ['book', 'journal'], :highlight => true
-    config.add_index_field 'abstract_ts', :helper_method => :render_abstract_with_highlights, :highlight => true, separator: ''
+    config.add_index_field 'abstract_ts', :helper_method => :render_highlighted_abstract, :highlight => true, separator: ''
     config.add_index_field 'issn_ss', :format => ['journal']
     config.add_index_field 'dissertation_date_ssf', :helper_method => :render_dissertation_date, :format => ['thesis']
 
+    ##
+    # SHOW FIELDS
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
     config.add_show_field 'subtitle_ts'
@@ -126,7 +106,6 @@ class CatalogController < ApplicationController
     config.add_show_field 'journal_title_ts', :format => ['article'], :helper_method => :render_journal_info_show
     config.add_show_field 'conf_title_ts', :helper_method => :render_conference_info_show
     config.add_show_field 'format', :helper_method => :render_type
-    #config.add_show_field 'subformat_s', :helper_method => :render_subtype
     config.add_show_field 'publisher_ts'
     config.add_show_field 'isbn_ss'
     config.add_show_field 'issn_ss'
@@ -138,76 +117,55 @@ class CatalogController < ApplicationController
     config.add_show_field 'dissertation_date_ssf', :helper_method => :render_dissertation_date, :format => ['thesis']
     config.add_show_field 'supervisor_ts', :helper_method => :render_author_links
 
+    ##
+    # SEARCH FIELDS
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
-    #
-    # Search fields will inherit the :qt solr request handler from
-    # config[:default_solr_parameters], OR can specify a different one
-    # with a :qt key/value. Below examples inherit, except for subject
-    # that specifies the same :qt as default for our own internal
-    # testing purposes.
-    #
-    # The :key is what will be used to identify this BL search field internally,
-    # as well as in URLs -- so changing it after deployment may break bookmarked
-    # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different.
-
-    # This one uses all the defaults set by the solr request handler. Which
-    # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise.
     config.add_search_field 'all_fields'
 
-    # Now we see how to over-ride Solr request handler defaults, in this
-    # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields.
     config.add_search_field 'title' do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params.
-      #field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
-
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
       field.solr_local_parameters = {
-        :qf => '$title_qf',
-        #:pf => '$title_pf'
+          :qf => '$title_qf',
+          #:pf => '$title_pf'
       }
     end
 
     config.add_search_field 'author' do |field|
       #field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
       field.solr_local_parameters = {
-        :qf => '$author_qf',
-        #:pf => '$author_pf'
+          :qf => '$author_qf',
+          #:pf => '$author_pf'
       }
     end
-
-    # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
 
     config.add_search_field 'numbers' do |field|
       field.include_in_simple_select = false
       field.solr_local_parameters = {
-        :qf => '$numbers_qf'
+          :qf => '$numbers_qf'
       }
     end
 
     config.add_search_field 'journal_title' do |field|
       field.include_in_simple_select = false
       field.solr_local_parameters = {
-        :qf => '$journal_title_qf'
+          :qf => '$journal_title_qf'
       }
     end
 
+    ##
+    # SORT FIELDS
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_tsort desc, journal_vol_tsort desc, journal_issue_tsort desc, journal_page_start_tsort asc, title_sort asc', :label => 'relevance'
-    config.add_sort_field 'pub_date_tsort desc, journal_vol_tsort desc, journal_issue_tsort desc, journal_page_start_tsort asc, title_sort asc', :label => 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
-    config.add_sort_field 'title_sort asc, pub_date_tsort desc', :label => 'title'
+
+    # sort fields Provided by Dtu::CatalogBehavior - relevance, year, title
+
+    author_ordering = [
+        'author_sort asc',
+        'title_sort asc'
+    ]
+    config.add_sort_field author_ordering.join(', '), :label => 'author'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
--- app/presenters/dtu/presenter.rb
+++
@@ -1,5 +0,0 @@
-module Dtu
-  class Presenter
-    include Dtu::PresenterBehavior
-  end
-end
