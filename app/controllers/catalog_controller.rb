c38c38041ca44a7ce14006b36597198af3e79079
Ronan McHugh <mchugh.r@gmail.com>
2016-04-01 12:55:10 +0200

    RM #2475 - change 'year' to 'publication year'

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -198,7 +198,7 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     config.add_sort_field 'score desc, pub_date_tsort desc, journal_vol_tsort desc, journal_issue_tsort desc, journal_page_start_tsort asc, title_sort asc', :label => 'relevance'
-    config.add_sort_field 'pub_date_tsort desc, journal_vol_tsort desc, journal_issue_tsort desc, journal_page_start_tsort asc, title_sort asc', :label => 'year'
+    config.add_sort_field 'pub_date_tsort desc, journal_vol_tsort desc, journal_issue_tsort desc, journal_page_start_tsort asc, title_sort asc', :label => 'publication year'
     config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
     config.add_sort_field 'title_sort asc, pub_date_tsort desc', :label => 'title'
 
