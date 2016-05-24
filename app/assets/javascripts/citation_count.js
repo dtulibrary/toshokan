840000e7cdf5c54bc66f91e3fcf2d902b9a611dc
Troels Lisberg Nielsen <troels.lisberg@gmail.com>
2016-05-24 09:05:16 +0200

    Updates citation_count.js

--- app/assets/javascripts/citation_count.js
+++ app/assets/javascripts/citation_count.js
@@ -1,14 +1,42 @@
 (function ($) {
 
     var CitationCount = function(data){
-        var count = function(data, provider) {
+        var count = function(data, provider, pretty_provider) {
             if (data[provider] == undefined) return;
             this.count = data[provider]['count'];
             this.url = data[provider]['url'];
-            this.text = "<div><a href='" + this.url + "'> Citation count " + provider + ": " + this.count + "</a></div>";
+            this.appendCitationCountToElement = function(element) {
+              if (!this.hasUrl() && !this.hasCount())
+                return;
+
+              element.append(this.citationCountHTML());
+            };
+            this.hasUrl = function() {
+              return this.url !== undefined && this.url !== null && this.url !== "";
+            };
+            this.hasCount = function() {
+              return this.count !== undefined && this.count !== null && this.count !== "";
+            };
+            this.citationCountHTML = function() {
+              return "<div>" + this.wrapInLink(this.text()) + "</div>"
+            };
+            this.text = function() {
+              return "Citation count " + pretty_provider + ": " + this.count;
+            };
+            this.wrapInLink = function(toBeWrapped) {
+              if (this.hasUrl()) {
+                return "<a href='" + this.url + "'>" + toBeWrapped + "</a>";
+              } else {
+                return toBeWrapped;
+              }
+            };
         };
-        this.elsevier = new count(data, 'elsevier');
-        this.wok = new count(data, 'wok');
+        this.elsevier = new count(data, 'elsevier', 'Elsevier');
+        this.wok = new count(data, 'web_of_science', 'Web of Science');
+        this.appendCitationCountToElement = function(element) {
+          this.elsevier.appendCitationCountToElement(element);
+          this.wok.appendCitationCountToElement(element);
+        };
     };
   $(function() {
       var $citationCountElement = $('#citation_count_lookup');
@@ -24,11 +52,10 @@
               pmid: pmid
           }}).then(
         function(data, textStatus, jqXHR) {
+            $citationCountElement.empty();
+
             var citeCount = new CitationCount(data);
-            var text = "";
-            if (!$.isEmptyObject(citeCount.elsevier)) text += citeCount.elsevier.text;
-            if (!$.isEmptyObject(citeCount.wok)) text += citeCount.wok.text;
-            $citationCountElement.empty().html(text);
+            citeCount.appendCitationCountToElement($citationCountElement);
         },
         function(jqXHR, textStatus, errorThrown) {
             if (window.console !== undefined) {
