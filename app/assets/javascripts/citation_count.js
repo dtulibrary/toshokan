81b4a500af770c3b4664a0b6557ebb0bf7d1d783
Troels Lisberg Nielsen <troels.lisberg@gmail.com>
2016-06-10 11:08:36 +0200

    Reformats some citation count code
    Reformats code based on Hound feedback.

--- app/assets/javascripts/citation_count.js
+++ app/assets/javascripts/citation_count.js
@@ -1,7 +1,7 @@
 (function ($) {
 
     var CitationCount = function(data){
-        var count = function(data, provider, pretty_provider) {
+        var Count = function(data, provider, prettyProvider) {
             if (data[provider] == undefined) return;
             this.count = data[provider]['count'];
             this.url = data[provider]['url'];
@@ -12,27 +12,27 @@
               element.append(this.citationCountHTML());
             };
             this.hasUrl = function() {
-              return this.url !== undefined && this.url !== null && this.url !== "";
+              return this.url !== undefined && this.url !== null && this.url !== '';
             };
             this.hasCount = function() {
-              return this.count !== undefined && this.count !== null && this.count !== "";
+              return this.count !== undefined && this.count !== null && this.count !== '';
             };
             this.citationCountHTML = function() {
-              return "<div>" + this.wrapInLink(this.text()) + "</div>"
+              return '<div>' + this.wrapInLink(this.text()) + '</div>';
             };
             this.text = function() {
-              return "Citation count " + pretty_provider + ": " + this.count;
+              return 'Citation count ' + prettyProvider + ': ' + this.count;
             };
             this.wrapInLink = function(toBeWrapped) {
               if (this.hasUrl()) {
-                return "<a href='" + this.url + "'>" + toBeWrapped + "</a>";
+                return "<a href='" + this.url + "'>" + toBeWrapped + '</a>';
               } else {
                 return toBeWrapped;
               }
             };
         };
-        this.elsevier = new count(data, 'elsevier', 'Elsevier');
-        this.wok = new count(data, 'web_of_science', 'Web of Science');
+        this.elsevier = new Count(data, 'elsevier', 'Elsevier');
+        this.wok = new Count(data, 'web_of_science', 'Web of Science');
         this.appendCitationCountToElement = function(element) {
           this.elsevier.appendCitationCountToElement(element);
           this.wok.appendCitationCountToElement(element);
@@ -44,11 +44,11 @@
       var api = $citationCountElement.data('api');
       var doi = $citationCountElement.data('doi');
       var pmid = $citationCountElement.data('pmid');
-      var scopus_id = $citationCountElement.data('scopus_id');
+      var scopusId = $citationCountElement.data('scopus_id');
       $.ajax(api, {
           data: {
               doi: doi,
-              scopus_id: scopus_id,
+              scopus_id: scopusId,
               pmid: pmid
           }}).then(
         function(data, textStatus, jqXHR) {
--- app/helpers/citation_count_helper.rb
+++ app/helpers/citation_count_helper.rb
@@ -1,15 +1,15 @@
 module CitationCountHelper
   def render_citation_count_text?(document)
-    citation_count_identifiers(document).size > 0
+    !citation_count_identifiers(document).empty?
   end
 
-  def citation_count_text(document, opts={})
+  def citation_count_text(document, opts = {})
     identifiers = citation_count_identifiers(document)
     api_path = Rails.configuration.getit[:url] + '/citation_count'
     content_tag(:div, '', id: 'citation_count_lookup', data: identifiers.merge(api: api_path))
   end
 
   def citation_count_identifiers(document)
-    mendeley_identifiers(document).keep_if { |k,_| k.in? [:doi, :scopus, :pmid]}
+    mendeley_identifiers(document).keep_if { |k, _| k.in? [:doi, :scopus, :pmid] }
   end
 end
