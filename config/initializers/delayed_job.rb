9bd1c21a162769611b72e206957093a7d5971eab
Ronan McHugh <mchugh.r@gmail.com>
2016-07-07 14:56:40 +0200

    reduce max retries

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,4 +2,5 @@ Rails.application.config.to_prepare do
   Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
   Delayed::Worker.destroy_failed_jobs = false
   Delayed::Worker.delay_jobs = Rails.application.config.delay_jobs
+  Delayed::Worker.max_attempts = 5
 end
