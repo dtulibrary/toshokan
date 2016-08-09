85983bbbb0a4387e2e7a0d895858b10e0ed12f40
viktorsmari <viktorsmari@gmail.com>
2016-08-09 10:25:01 +0200

    Adding correct tests for test script

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
-rake db:migrate
-rspec
+bundle exec rake db:setup
+bundle exec rake db:test:prepare
+bundle exec rake jetty:start
+bundle exec rake metastore:testdata:index
+
+# should we run Cucumber tests on jenkins?
+bundle exec rake
 
