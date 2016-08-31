4eb3017f5c7ca60d0c637405870a7107d4fc726c
Troels Lisberg Nielsen <troels.lisberg@gmail.com>
2016-08-31 10:31:43 +0200

    Cucumber: Remove altmetric test

--- features/step_definitions/altmetric_steps.rb
+++
@@ -1,3 +0,0 @@
-Then(/^I should(n't| not)? see the altmetric badge for doi "(.*?)"$/) do |negate, doi|
-  expect( page ).send( (negate ? :to_not : :to), have_xpath("//div[@class=\"altmetric-embed\" and @data-doi=\"#{doi}\"]") )
-end
