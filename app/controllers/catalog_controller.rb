edb17bae6651c0190f0c0a7a871e7723561a1158
Ronan McHugh <mchugh.r@gmail.com>
2016-10-18 14:56:33 +0200

    #RM2659 - reintroduces pubdate slider

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -83,7 +83,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'fulltext_availability_ss', label: 'Online Access', helper_method: :online_access_facet_display, collapse: false, if: :show_online_access_facet?
     config.add_facet_field 'format', :collapse => false
     #config.add_facet_field 'subformat_s', :collapse => false
-    # config.add_facet_field 'pub_date_tsort', :label => I18n.t('blacklight.search.fields.facet.pub_date_tsort'), :range => true
+    config.add_facet_field 'pub_date_tsort', :label => I18n.t('blacklight.search.fields.facet.pub_date_tsort'), :range => true
     config.add_facet_field 'author_facet', :limit => 20
     config.add_facet_field 'journal_title_facet', :limit => 20
     config.add_facet_field 'isolanguage_facet', :limit => 20
