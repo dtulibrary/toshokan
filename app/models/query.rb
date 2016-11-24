d0318feab4b7c9dd94ab23b80792e6976a28e21c
Jimmy Petersen <jipe@dtu.dk>
2016-11-24 09:13:30 +0100

    Add options to adjust the common filter queries

--- app/models/query.rb
+++ app/models/query.rb
@@ -6,21 +6,33 @@ class Query < ActiveRecord::Base
 
   FORMATS           = ['article', 'book', 'other']
   PUBLICATION_YEARS = [-1, 0, 1]
+  ACCESS            = ['dtu']
 
-  def self.common_filter_query
+  # Params:
+  #   options[:formats]           : which formats to search for
+  #   options[:publication_years] : which publication years (offsets from current year) to search for
+  #   options[:access]            : which access modifiers to search for
+  def self.common_filter_query(options = {})
+    options = {
+      :formats           => FORMATS,
+      :publication_years => PUBLICATION_YEARS,
+      :access            => ACCESS
+    }.merge(options)
+
     current_year = Time.new.year
+
     [
-      "format:(#{FORMATS.join(' OR ')})",
-      "pub_date_tis:(#{PUBLICATION_YEARS.map {|y| current_year + y}.join(' OR ')})",
-      "access_ss:dtu"
+      "format:(#{options[:formats].join(' OR ')})",
+      "pub_date_tis:(#{options[:publication_years].map {|y| current_year + y}.join(' OR ')})",
+      "access_ss:(#{options[:access].join(' OR ')})"
     ]
   end
 
   def self.normalize(query_string)
-    query_string.gsub(/^\s+|\s+$/, '')
-                .gsub(/\s+/, ' ')
+    query_string.strip.squeeze
   end
 
+  # Combine this query and the common filter query
   def to_solr_query
     [::Query.normalize(query_string), ::Query.common_filter_query].flatten.join(' AND ')
   end
