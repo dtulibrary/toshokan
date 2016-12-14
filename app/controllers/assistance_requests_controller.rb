dtulibrary/toshokan

commit a0bd947199a463b1d37d4edbb41595a2b918b2dc

Fix Assistance Request links RM #2869

app/controllers/assistance_requests_controller.rb
@@ -152,42 +152,6 @@ class AssistanceRequestsController < ApplicationController
     params[:genre].to_sym if params[:genre]
   end
 
-  def determine_assistance_request_genre(document)
-    # No CFF for journals
-    return if document['format'] == 'journal'
-    # No CFF for printed and electronic books (unspecified books have CFF)
-    return if document['format'] == 'book' && ['printed', 'ebook'].include?(document['subformat_s'])
-    # No pre-populated CFF for journal articles
-    return if document['format'] == 'article' && document['subformat_s'] == 'journal_article'
-
-    return :thesis if document['format'] == 'thesis'
-
-    if document.has_key?('subformat_s')
-      case document['subformat_s']
-      when 'report'
-        :report
-      when 'standard'
-        :standard
-      when 'patent'
-        :patent
-      when 'journal_article'
-        :journal_article
-      when 'conference_paper'
-        :conference_article
-      else
-        :other
-      end
-    else
-      case document['format']
-      when 'article'
-        :journal_article
-      when 'book'
-        :book
-      when 'other'
-        :other
-      end
-    end
-  end
 
   # Returns a proc that will get the contents of a doc field or return '?'
   # Used to populate required fields

app/helpers/assistance_requests_helper.rb
@@ -1,6 +1,6 @@
 module AssistanceRequestsHelper
-  
-  # Renders a label and a text field. 
+
+  # Renders a label and a text field.
   # Add :required => true to make the field required
   def labeled_text_field object_name, field_name, field_label, options = {}
     param_name = "#{object_name}[#{field_name}]"
@@ -23,7 +23,7 @@ module AssistanceRequestsHelper
         cssClass += ' error'
       end
       label_tag(param_name, "#{field_label} <span class=\"required-label\">(<i>required</i>)</span>".html_safe, :class => cssClass)
-    else 
+    else
       label_tag(param_name, "#{field_label} <span class=\"required-label hide\">(<i>required</i>)</span>".html_safe)
     end
   end
@@ -31,7 +31,7 @@ module AssistanceRequestsHelper
   def labeled_text_area object_name, field_name, field_label, options = {}
     param_name = "#{object_name}[#{field_name}]"
     options[:class] = 'required' if options[:required]
-    label_for(param_name, object_name, field_name, field_label, options) + 
+    label_for(param_name, object_name, field_name, field_label, options) +
     text_area_tag(param_name, params[object_name] && params[object_name][field_name], options)
   end
 
@@ -101,4 +101,24 @@ module AssistanceRequestsHelper
     )
   end
 
+  # Create a link to an assistance request form
+  # If we have the doc we just supply the cluster_id
+  # If there is no cluster id, we don't have the doc in the index
+  # so we parse the doc values to create a pre-filled request form
+  def assistance_request_link(document)
+    if document['cluster_id_ss'].present?
+      new_assistance_request_path(:record_id => document['cluster_id_ss'].first)
+    else
+      # get the right params for an assistance request
+      # wrap the params so they can be parsed by the assistance request form
+      genre = determine_assistance_request_genre(document)
+      req_params = AssistanceRequest.params_for_doc(document)
+      params = Hash[req_params.map{|k,v| ["assistance_request[#{k}]", v]}].merge(:genre => genre)
+      new_assistance_request_path(params)
+    end
+  end
+
+  def determine_assistance_request_genre(document)
+    AssistanceRequest.request_genre(document)
+  end
 end

app/models/assistance_request.rb
@@ -147,4 +147,74 @@ class AssistanceRequest < ActiveRecord::Base
     end
     co
   end
+
+  # Given a SolrDocument
+  # Map its values to a param hash suitable for
+  # use in the relevant assistance request form.
+  # See .form_sections method above for the different
+  # form types.
+  def self.params_for_doc(document)
+    genre = request_genre(document)
+    # transform the doc values so that keys are strings and values are singular
+    rft_params = Hash[
+      document.to_semantic_values.map do |k,v|
+        val = v.class == Array ? v.first : v
+        [k.to_s, val]
+      end
+    ]
+
+    # Loop through the relevant sections -
+    # if openurl name for a key is present in our params
+    # then we should use the value, with the key
+    # specified in the section
+    params = {}
+    form_sections[genre.to_s].each do |section|
+      ou_key = section[:ou]
+      if ou_key.in? rft_params.keys
+        param_key = section[:name]
+        param_val = rft_params[ou_key]
+        params[param_key] = param_val
+      end
+    end
+    params
+  end
+
+  # Determine the appropriate request genre
+  # for a SolrDocument.
+  def self.request_genre(document)
+    # No CFF for journals
+    return if document['format'] == 'journal'
+    # No CFF for printed and electronic books (unspecified books have CFF)
+    return if document['format'] == 'book' && ['printed', 'ebook'].include?(document['subformat_s'])
+    # No pre-populated CFF for journal articles
+    return if document['format'] == 'article' && document['subformat_s'] == 'journal_article'
+
+    return :thesis if document['format'] == 'thesis'
+
+    if document.has_key?('subformat_s')
+      case document['subformat_s']
+      when 'report'
+        :report
+      when 'standard'
+        :standard
+      when 'patent'
+        :patent
+      when 'journal_article'
+        :journal_article
+      when 'conference_paper'
+        :conference_article
+      else
+        :other
+      end
+    else
+      case document['format']
+      when 'article'
+        :journal_article
+      when 'book'
+        :book
+      when 'other'
+        :other
+      end
+    end
+  end
 end

app/views/catalog/_show_fulltext.html.erb
@@ -19,7 +19,7 @@
           <small>Request document from DTU Library Document Supply Service.</small>
         </p>
         <div class="fulltext-action">
-          <%= link_to 'Request', new_assistance_request_path(:record_id => @document['cluster_id_ss'].first), :class => 'btn btn-primary btn-small' %>
+          <%= link_to 'Request', assistance_request_link(@document), :class => 'btn btn-primary btn-small' %>
         </div>
       </li>
     <% end %>

spec/helpers/assistance_requests_helper_spec.rb
@@ -1,0 +1,16 @@
+require 'rails_helper'
+describe AssistanceRequestsHelper do
+  describe 'assistance_request_link' do
+    subject { helper.assistance_request_link(document) }
+    context 'when it is a synthetic document' do
+      let(:document){ SolrDocument.new(title_ts:'ELECTROCHEMICAL SCIENCE AND TECHNOLOGY OF COPPER, PROCEEDINGS', format: 'book')}
+      it { should include 'genre=book' }
+      it { should include CGI.escape('assistance_request[book_title]')}
+      it { should include CGI.escape('ELECTROCHEMICAL SCIENCE AND TECHNOLOGY') }
+    end
+    context 'when it is a document in the index' do
+      let(:document) { SolrDocument.new(cluster_id_ss: ['5983']) }
+      it { should include 'record_id=5983' }
+    end
+  end
+end

spec/models/assistance_request_spec.rb
@@ -9,4 +9,13 @@ describe AssistanceRequest do
       expect(ar.openurl.kev).to include "rft.atitle=test+title&rft.jtitle=test+journal+title&rft.volume=1&rft.issue=2&rft.date=2014&rft.pages=34&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal"
     end
   end
+
+  describe 'params_for_doc' do
+    let(:document){ SolrDocument.new(title_ts:'ELECTROCHEMICAL SCIENCE AND TECHNOLOGY OF COPPER, PROCEEDINGS', format: 'book')}
+    it 'creates the correct parameters from an open_url' do
+      params = AssistanceRequest.params_for_doc(document)
+      expect(params['book_title']).to eq 'ELECTROCHEMICAL SCIENCE AND TECHNOLOGY OF COPPER, PROCEEDINGS'
+    end
+  end
+
 end

commit 24a948fc6da0f4ef80179d4440d09cab62ebdd91

Fixes regression introduced by a0bd947

app/controllers/assistance_requests_controller.rb
@@ -4,6 +4,7 @@ class AssistanceRequestsController < ApplicationController
 
   include Toshokan::PerformsSearches
   include Toshokan::Resolver
+  include AssistanceRequestsHelper
 
 
   def index